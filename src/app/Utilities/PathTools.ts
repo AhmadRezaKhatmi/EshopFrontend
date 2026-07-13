c98b374cd8be1ce78a05ae0a5a62bec9203a7cd9
ah-khatmi <ah-khatmi@88077-T2SH>
2026-07-13 15:23:15 +0330

    Add Product Galleries

--- src/app/Utilities/PathTools.ts
+++ src/app/Utilities/PathTools.ts
@@ -2,3 +2,4 @@ import {environment} from '../../environments/environment';
 
 export const DomainName = environment.production ? 'https://toplearn.com' : 'https://localhost:44381';
 export const ImagePath = DomainName + '/images/products/origin/';
+export const ImageGalleryPath = DomainName + '/images/product-galleries/';

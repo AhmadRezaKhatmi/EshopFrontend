0d80f1c9a64775e39971c5d374dfc83f531bb57d
ah-khatmi <ah-khatmi@88077-T2SH>
2026-07-15 11:55:13 +0330

    Add 'Sabade Kharid'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from './services/auth.service';
 import {CurrentUser} from './DTOs/Account/CurrentUser';
+import {OrderService} from './services/order.service';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,8 @@ export class AppComponent implements OnInit {
   title = 'eshop-pro';
 
   constructor(
-    private authService: AuthService
+    private authService: AuthService,
+    private orderService: OrderService
   ) {
   }
 
@@ -25,6 +27,12 @@ export class AppComponent implements OnInit {
           res.data.address);
 
         this.authService.setCurrentUser(user);
+      }
+    });
+
+    this.orderService.getUserBasketDetails().subscribe(res => {
+      if (res.status === 'Success') {
+        this.orderService._setOrderDetails(res.data);
       }
     });
   }
